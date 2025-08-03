f0cd5c94a849597bd1f0a6ecc23e89aa0540f423
onnojansen <onno.jansen@proact.nl>
2025-08-03 20:19:13 +0200

    cleanup

--- models.go
+++ models.go
@@ -1,46 +1,46 @@
 package azurenamingtool
 
-// Order -
-type Order struct {
-	ID    int         `json:"id,omitempty"`
-	Items []OrderItem `json:"items,omitempty"`
-}
+// // Order -
+// type Order struct {
+// 	ID    int         `json:"id,omitempty"`
+// 	Items []OrderItem `json:"items,omitempty"`
+// }
 
-// OrderItem -
-type OrderItem struct {
-	Coffee   Coffee `json:"coffee"`
-	Quantity int    `json:"quantity"`
-}
+// // OrderItem -
+// type OrderItem struct {
+// 	Coffee   Coffee `json:"coffee"`
+// 	Quantity int    `json:"quantity"`
+// }
 
-// Coffee -
-type Coffee struct {
-	ID          int                `json:"id"`
-	Name        string             `json:"name"`
-	Teaser      string             `json:"teaser"`
-	Collection  string             `json:"collection"`
-	Origin      string             `json:"origin"`
-	Color       string             `json:"color"`
-	Description string             `json:"description"`
-	Price       float64            `json:"price"`
-	Image       string             `json:"image"`
-	Ingredient  []CoffeeIngredient `json:"ingredients"`
-}
+// // Coffee -
+// type Coffee struct {
+// 	ID          int                `json:"id"`
+// 	Name        string             `json:"name"`
+// 	Teaser      string             `json:"teaser"`
+// 	Collection  string             `json:"collection"`
+// 	Origin      string             `json:"origin"`
+// 	Color       string             `json:"color"`
+// 	Description string             `json:"description"`
+// 	Price       float64            `json:"price"`
+// 	Image       string             `json:"image"`
+// 	Ingredient  []CoffeeIngredient `json:"ingredients"`
+// }
+
+// // Ingredient -
+// type CoffeeIngredient struct {
+// 	ID       int    `json:"ingredient_id"`
+// 	Name     string `json:"name"`
+// 	Quantity int    `json:"quantity"`
+// 	Unit     string `json:"unit"`
+// }
 
 // Ingredient -
-type CoffeeIngredient struct {
-	ID       int    `json:"ingredient_id"`
-	Name     string `json:"name"`
-	Quantity int    `json:"quantity"`
-	Unit     string `json:"unit"`
-}
-
-// Ingredient -
-type Ingredient struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Quantity int    `json:"quantity"`
-	Unit     string `json:"unit"`
-}
+// type Ingredient struct {
+// 	ID       int    `json:"id"`
+// 	Name     string `json:"name"`
+// 	Quantity int    `json:"quantity"`
+// 	Unit     string `json:"unit"`
+// }
 
 // ResourceTypes -
 type ResourceTypes struct {
